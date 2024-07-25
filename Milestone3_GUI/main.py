0bacb5a2ec56d222b6a416bffd55a75a2ea85b4b
Andrew Lewis <123277732+Andrewlewis7@users.noreply.github.com>
2024-07-24 20:24:32 -0600

    remove old files

--- Milestone3_GUI/main.py
+++
@@ -1,8 +0,0 @@
-from simulator_class import Simulator
-from simulatorGUI_class import UVsim
-
-if __name__ == "__main__":
-    gui = UVsim(None)
-    simulator = Simulator(gui.output_function)
-    gui.simulator = simulator
-    gui.start()
--- Milestone3_GUI/program_loader_class.py
+++
@@ -1,6 +0,0 @@
-class ProgramLoader:
-    @staticmethod
-    def load_program_from_file(file_path):
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-        return [int(line.strip()) for line in lines if line.strip()]
